a4683c674672bc6b326535d7f82e99ac21df037d
knp84 <sabanovroman84@gmail.com>
2024-06-28 00:02:09 +0500

    Update Project.py

--- Project.py
+++ Project.py
@@ -58,57 +58,97 @@ while True:
                 print('Верный ответ!')
             else:
                 print('Неверный ответ!', x)
-        case '4':
+        case '4':    
             a = randint(1,9)
             b = randint(-9,9)
             c = randint(-9,9)
-            sign1 = ''
-            sign2 = ''
-            if b >= 0:
-                sign1 = '+'
-            if c >= 0:
-                sign2 = '+'    
-            list = tuple(range(1, 101)) 
+            spic = tuple(range(1, 101)) 
             D = (b ** 2) - (4 * a * c)
-            if D > 0:
-                x1 = (-b - sqrt(D)) / (2 * a)
-                x2 = (-b + sqrt(D)) / (2 * a)
-                determine = 3
-                if sqrt(D) in list:                #ограничивает дискриминант
-                    print(f'{a}x^2 {sign1}{b}x {sign2}{c} = 0')
-                    x1x2 = f'{min(x1, x2)}, {max(x1, x2)}'
-                    player_choise1 = input('Введите дискриминант квадратного уравнения: ')
-                    if player_choise1 == str(D):
-                        player_choise2 = input('Верно! Введите x1 и x2 в порядке возрастания через запятую: ')
-                        if player_choise2 == x1x2:
-                            print('Правильно!', x1x2) 
+            def square_equation():
+                sign1 = ''
+                sign2 = ''
+                if b >= 0:
+                    sign1 = '+'
+                if c >= 0:
+                    sign2 = '+' 
+                print(f'{a}x^2 {sign1}{b}x {sign2}{c} = 0', D)
+            def player_solution():
+                match determine:                                 
+                    case 1:
+                        try:
+                            solution = int(input())     
+                            return solution                               
+                        except:                                                            
+                            return print('Ошибка ввода! Введите число!')                
+                    case 2:
+                        try:
+                            solution = float(input('округлите x до первого знака после запятой '))
+                            return solution
+                        except:
+                            print('Введите число с плавающей точкой!')
+                    case 3:                                #решил  убрать не нужные точки (1.0, 5.0) и расширить функцию. Работает так себе
+                        player_x = input()
+                        try:
+                            player_x = float(player_x)
+                            if player_x == int(player_x):
+                                return int(player_x)
+                            else:
+                                return round(player_x, 1)
+                        except:
+                            print('err')         
+            if D > 0:                               
+                if sqrt(D) not in spic:
+                    while True:              
+                        j = randint(-1,1)               #изменяю a b c для удовлетворительного значения D
+                        a += j 
+                        b += j 
+                        c += j 
+                        D = (b ** 2) - (4 * a * c)
+                        if D > 0:
+                            if sqrt(D) in spic and a > 0:
+                                print(a, b, c, D)
+                                break
                         else:
-                            print('Неправильно!', x1x2)
-                    else:
-                        print(f'Неверно, дискриминант равен {D}')  
-            elif D == 0:
-                x = -b / (2 * a)
-                print(f'{a}x^2 {sign1}{b}x {sign2}{c} = 0')
-                print('Введите дискриминант квадратного уравнения: ')
+                            print('pon')
+                            break    
+                if D > 0:
+                    if sqrt(D) in spic:
+                        x1 = (-b - sqrt(D)) / (2 * a)
+                        x2 = (-b + sqrt(D)) / (2 * a)
+                        determine = 1
+                        square_equation()
+                        if player_solution() == D:     
+                            print(x1, x2, round(x1, 1), round(x2, 1))
+                            determine = 3
+                            if player_solution() == round(x1, 1) or player_solution() == int(x1):
+                                if player_solution() == round(x2, 1) or player_solution() == int(x2): #странно работает если допустить ошибку в x2
+                                    print('Верно!')
+                                else:
+                                    print('Неверно!')
+                            else:
+                                print('Неверно!')        
+                        else:               
+                            print('Неверно!')
+            if D < 0:
+                square_equation()
                 determine = 1
-                if player_solution() == D:
-                    player_choise2 = input('Введите x1 и x2 квадратного кравнения: ')
-                    if player_choise2 == str(x):
-                        print('Вы правильно решили квадратное уравнение!') 
-                    else:
-                        print('Вы неправильно решили квадратное уравнение! Попробуйте сонва!')
+                if D == player_solution():
+                    print('правильно, корней нет')
                 else:
-                    print(f'Неверно, дискриминант равен {D}') 
-            elif D < 0:
-                x = 'Корней нет'
+                    print('Неверно!')
+            if D == 0:
+                square_equation()
                 determine = 1
-                print(f'{a}x^2 {sign1}{b}x {sign2}{c} = 0', 'Введите дискриминант уравнения: ', sep='\n')
-                if player_solution() == D:
-                    print('Правильно! Корней нет')
+                x = (-b - sqrt(D)) / (2 * a)
+                if D == player_solution():
+                    determine = 3
+                    print('Введите корень:')
+                    if x == player_solution():
+                        print('Верно!')
+                    else:
+                        print('Неверно!')
                 else:
-                    print(f'Неверно, дискриминант равен {D}')
-            else:
-                print('error') #подстраховка
+                    print('Неверно')
         case _:
             print('Выберете один из существующих уровней сложности!')
     end_cycle = input('Хотите попробовать еще раз? ')
