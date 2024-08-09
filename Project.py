3ee036a8db74c61afea62138b9ae0af69120c938
knp84 <sabanovroman84@gmail.com>
2024-08-09 19:47:58 +0500

    Update Project.py

--- Project.py
+++ Project.py
@@ -23,7 +23,17 @@ def player_solution():
                 return solution
             except:
                 print('Введите число с плавающей точкой!')
-        
+        case 3:                                #решил  убрать не нужные точки (1.0, 5.0) и расширить функцию. Работает так себе
+            player_x = player_xz
+            try:
+                player_x = float(player_x)
+                if player_x == int(player_x):
+                    return int(player_x)
+                else:
+                    return round(player_x, 1)
+            except:
+                print('err') 
+                return player_x         
 if D > 0:                               
     if sqrt(D) not in spic:
         while True:              
@@ -55,8 +65,13 @@ if D > 0:
             if player_solution() == D:     
                 print(x1, x2, round(x1, 1), round(x2, 1))
                 determine = 3
-                if player_solution() == round(x1, 1) or player_solution() == int(x1):
-                    print('Верно!')
+                player_xz = input()
+                if player_solution() == int(x1) or player_solution() == round(x1, 1):
+                    player_xz = input()
+                    if player_solution() == int(x2) or player_solution() == round(x2, 1):
+                        print('Верно!')
+                    else: 
+                        print('Неверно!')
                 else:
                     print('Неверно!')        
             else:               
